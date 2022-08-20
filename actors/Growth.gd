d4630f884a2113e76542c99544e0143862954f41
Bloodyaugust <greysonrichey@gmail.com>
2022-08-20 03:17:34 -0600

    Tweakin data values and foliage placement

--- actors/Growth.gd
+++ actors/Growth.gd
@@ -16,9 +16,9 @@ export var starting_asteroid: NodePath
 var asteroid: Node2D
 
 onready var _connections: Node2D = $"%Connections"
-onready var _foliage: Node2D = $"%Foliage"
 
 var _asteroid_data: AsteroidData
+var _foliage: Node2D
 
 func _process(delta):
   for _child in _foliage.get_children():
@@ -32,14 +32,16 @@ func _ready():
     
   _asteroid_data = asteroid.data as AsteroidData
   
-  var _num_foliage: int = (randi() % 6) + 1
+  _foliage = asteroid.find_node("Foliage", true, false)
+  
+  var _num_foliage: int = (randi() % 6) + 1 + _asteroid_data.size
   for _i in range(_num_foliage):
     var _new_foliage: Sprite = Sprite.new()
-    var _new_position: Vector2 = global_position + (Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * (_asteroid_data.size * rand_range(5, 15)))
+    var _new_position: Vector2 = global_position + (Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * (_asteroid_data.size * rand_range(20.0, 25.0)))
 
     _new_foliage.texture = FOLIAGE_SPRITES[randi() % FOLIAGE_SPRITES.size()]
     _new_foliage.scale = Vector2(0.4, 0.4)
     _new_foliage.global_position = _new_position
     _new_foliage.rotation = global_position.angle_to_point(_new_position) - PI / 2
-    _new_foliage.set_meta("scale_offset", rand_range(0, 1000))
+    _new_foliage.set_meta("scale_offset", rand_range(0, 10000))
     _foliage.add_child(_new_foliage)
